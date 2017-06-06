34509c5fb7f7cdd044765a5287af75a293203430
dltbg <dtobing81@gmail.com>
2017-06-06 11:43:20 +0700

    Dashboard_admin

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('dashboard', function () {
+    return view('dashboard');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+
+
