f3015e1e883a94ebf315cf4a657ec5a47b6b8986
Jonathan Rehuel Lewerissa <joel.sembilan8@gmail.com>
2019-02-14 19:09:19 +0700

    Add config files for laravel-permission

--- database/migrations/frontdb/2017_06_25_160927_create_presensis_table.php
+++ database/migrations/frontdb/2017_06_25_160927_create_presensis_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAbsensisTable extends Migration
+class CreatePresensisTable extends Migration
 {
     /**
      * Run the migrations.
@@ -32,6 +32,6 @@ class CreateAbsensisTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('absensis');
+        Schema::dropIfExists('presensis');
     }
 }
